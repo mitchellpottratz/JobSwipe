edfdbc23e9e338e3b3209e159032bd25f173c1ce
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-16 12:44:56 -0600

    created a main server class

---
+++ server.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+
+
+# allows flask to access enviroment variables
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env') 
+load_dotenv(dotenv_path)
+
+
+class Server:
+
+    def __init__(self, blueprints):
+        self.app = Flask(__name__)
+        self.DEBUG = os.environ['DEBUG']
+        self.PORT = os.environ['PORT']
+        self.origin = self.set_origin()    
+        self.app.secret_key = os.environ['SECRET_KEY']
+
+    # sets the origin to either development or production depending on 
+    # if DEBUG is set to true
+    def set_origin(self):
+        if os.environ['DEBUG']:
+            return os.environ['DEVELOPMENT_ORIGIN']
+        else:
+            return os.environ['PRODUCTON_ORIGIN']
+
+    def start(self): 
+        self.app.run(debug=self.DEBUG, port=self.PORT)
+        print("Server is running on port", self.PORT)
+
+    
