5c2b46e52575af11dfadfdf8b76c4fef8093f3ba
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-16 12:57:36 -0600

    created db connection method for handling requests

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import g
 from server import Server 
 from database import Database
 
@@ -5,6 +6,22 @@ from database import Database
 # creates instances of the server and database
 server = Server([])
 database = Database([])
+
+
+# gets the flask app object from the server so it can be used as a decorator
+app = server.app
+
+# established connection to the database before every request
+@app.before_request
+def before_request():
+    g.db = database.DATABASE
+    g.db.connect()
+
+# closes the database and returnt the response for every request
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 server.start()
 
