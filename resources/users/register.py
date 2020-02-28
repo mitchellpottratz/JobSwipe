88b0256b6df42adbf6209ab3d6bea03923f35894
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-27 20:19:56 -0600

    candidate users can register

--- resources/users/register.py
+++ resources/users/register.py
@@ -38,21 +38,47 @@ class Register(View):
         # if a user with the email does not already exist
         except DoesNotExist:
 
-            # if they are registering as a company user
+            # since company users and candidate users use different models they need to be
+            # created individually
             if data['is_company_user']:      
-                self.register_company_user(data)
-            
+                new_company_user = self.register_company_user(data)
+                response_data = new_company_user
+            else:
+                new_candidate_user = self.register_candidate_user(data)
+                response_data = new_candidate_user
+
+            return jsonify(
+                data=response_data,
+                status={
+                    'code': 201,
+                    'message': 'Resource created successfully.'
+                }
+            )
 
     def register_company_user(self, data):
         data['password'] = generate_password_hash(data['password'])
 
         new_company_user = CompanyUser.create(**data)
         new_company_user.generate_email_confirmation_code()
+        self.handle_profile_image_upload()
+        new_company_user.image = self.uploaded_images_path
         new_company_user.save()
 
-        self.
+        return new_company_user
 
+    
+    def register_candidate_user(self, data):
+        data['password'] = generate_password_hash(data['password'])
 
+        new_candidate_user = CandidateUser.create(**data)
+        new_candidate_user.generate_email_confirmation_code()
+        self.handle_profile_image_upload()
+        new_candidate_user.image = self.uploaded_images_path
+        new_candidate_user.save()
+
+        return new_candidate_user
+
+ 
     def handle_profile_image_upload(self):
         # gets the image from the form data if it exists
         if 'image' in request.files:
