ac039d0ab350fd99cf680305b36bfd87b75d01ae
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-29 09:34:26 -0600

    changed all fields to null

--- models/candidate_user.py
+++ models/candidate_user.py
@@ -6,10 +6,10 @@ This model is for users that are searching for jobs
 '''
 
 class CandidateUser(User):
-    location = CharField(max_length=255)
-    headline = CharField(max_length=100)
-    bio = CharField(max_length=500)
-    phone_number = CharField(max_length=12)
+    location = CharField(max_length=255, null=True)
+    headline = CharField(max_length=100, null=True)
+    bio = CharField(max_length=500, null=True)
+    phone_number = CharField(max_length=12, null=True)
 
 
 
